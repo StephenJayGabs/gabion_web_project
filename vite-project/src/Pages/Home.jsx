f1cd1e96d0886dbdaf4cf3aea438f3a16538ec84
StephenJayGabs <sgabion45@gmail.com>
2024-12-18 14:50:52 +0800

    my first commit

---
+++ vite-project/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+// import { useContext } from "react";
+// import { AppContext } from "../Context/AppContext";
+
+export default function Home(){
+    
+
+
+    return(
+        <>
+            <h1 className="title">Latest Post </h1>
+        </>
+    ); 
+}
